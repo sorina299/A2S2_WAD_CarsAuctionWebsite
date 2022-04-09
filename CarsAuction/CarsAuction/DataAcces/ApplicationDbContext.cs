b03cb9f0aacb8b133e755c0361fa17b0622bbb3d
sorina299 <hirca.sorina@gmail.com>
2022-04-10 00:41:26 +0300

    added dbcontext

---
+++ CarsAuction/CarsAuction/DataAcces/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CarsAuction.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarsAuction.DataAcces;
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<UserType> UserTypes  { get; set; }
+    public DbSet<Auction> Auctions  { get; set; }
+    public DbSet<Car> Cars  { get; set; }
+    public DbSet<Brand> Brands{ get; set; }
+    public DbSet<Model> Models{ get; set; }
+}
+
